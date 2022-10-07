2a74e69ec1a7bb90b85ad6cd670a69b3be613eff
inscripoem <1130067732@qq.com>
2022-10-07 21:34:25 +0800

    修复摸鱼插件

--- src/plugins/moyu/__init__.py
+++ src/plugins/moyu/__init__.py
@@ -1,22 +1,25 @@
 from nonebot import on_command, logger, get_driver
 from nonebot.params import CommandArg
 from nonebot.adapters.onebot.v11.bot import Bot
-from nonebot.adapters.onebot.v11 import MessageEvent, Message
-from .data_source import get_moyu
+from nonebot.adapters.onebot.v11 import MessageSegment, Message, MessageEvent
+import base64
+from re import findall
+from sys import exc_info
+import httpx
+from httpx import AsyncClient
+from nonebot import logger
 
 moyu = on_command("摸鱼", block=True)
 
 
 @moyu.handle()
-async def hf(bot: Bot, event: MessageEvent, msg: Message = CommandArg()):
-    pic = await get_moyu()
-    if pic[0]:
+async def hf(bot:Bot, event:MessageEvent):
+    async with httpx.AsyncClient() as client:
+        req_url = "https://api.j4u.ink/v1/store/other/proxy/remote/moyu.json"
         try:
-            await moyu.finish(message=Message(pic[2]))
-        except Exception as e:
-            logger.warning(e)
-            await moyu.finish(
-                message=Message(f"消息被风控，图发不出来\n{pic[1]}\n这是链接\n{pic[3]}"),
-            )
-    else:
-        await moyu.finish(f"出错：{pic[1]}")
+            res = await client.get(req_url, timeout=120)
+            logger.info(res.json())
+        except httpx.HTTPError as e:
+            logger.warning(f'API异常{e}')
+        img_url = res.json()['data']['moyu_url']
+        await bot.send(event=event, message=MessageSegment.image(img_url,cache=1))
